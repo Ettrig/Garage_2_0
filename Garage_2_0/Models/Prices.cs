0415c9aad2a7d53d722d4e738a995f8ad74129d4
Ettrig <rolf.engstrand@gmail.com>
2019-05-28 08:37:49 +0200

    Ett par småsaker

--- Garage_2_0/Models/Prices.cs
+++ Garage_2_0/Models/Prices.cs
@@ -1,9 +1,12 @@
-﻿namespace Garage_2_0.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Garage_2_0.Models
 {
     public class Prices
     {
         public int Id { get; set; }
         public VehicleType Type { get; set; }
+        [Range (0, 1000)]
         public int Price { get; set; }
     }
 }
