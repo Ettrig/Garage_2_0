14d98ce2e57d83434da1d914e060abfb297811eb
TB <tb@example.com>
2019-06-04 14:05:55 +0200

    Seed for 100 random Members

--- Garage_2_0/Program.cs
+++ Garage_2_0/Program.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Garage_2_0.Data;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace Garage_2_0
@@ -14,7 +16,16 @@ namespace Garage_2_0
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            IWebHost webHost = CreateWebHostBuilder(args).Build();
+
+            using (var scope = webHost.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                //Seed
+                SeedData.Initialize(services);
+            }
+
+            webHost.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
